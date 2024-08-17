724881baaedcc0b0d737ffe34982e9170db645db
DanFogelberg <dan.f.fogelberg@gmail.com>
2024-08-17 17:46:54 +0200

    Basic routes

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-400">
-      <h1 className="font-bold text-4xl text-black">Dan's Euler Playground</h1>
+    <main className="min-h-screen p-24 bg-slate-400">
+      <h1 className="font-bold text-4xl text-black mb-12">Dan's Euler Playground</h1>
+      <ul>
+        <li><a href="/tests/1">Test 1</a></li>
+        <li><a href="/tests/2">Test 2</a></li>
+      </ul>
     </main>
   );
 }
---
+++ app/tests/1/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-400">
+      <h1 className="font-bold text-4xl text-black">Test 1 Placeholder</h1>
+    </main>
+  );
+}
