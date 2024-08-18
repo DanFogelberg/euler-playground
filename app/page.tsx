fdcd2fa6e876339449997867a2e93bb0b3ef81c1
DanFogelberg <dan.f.fogelberg@gmail.com>
2024-08-18 22:39:55 +0200

    Rename tests problems

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <main className="min-h-screen p-24 bg-slate-400">
       <h1 className="font-bold text-4xl text-black mb-12">Dan's Euler Playground</h1>
       <ul>
-        <li><a href="/tests/1">Test 1</a></li>
-        <li><a href="/tests/2">Test 2</a></li>
+        <li><a href="/problem/1">Problem 1</a></li>
+        <li><a href="/problem/2">Problem 2</a></li>
       </ul>
     </main>
   );
